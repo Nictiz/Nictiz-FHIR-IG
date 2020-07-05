405e2d082075d80c3972825459f775a8f35c07db
Alexander Henket <henket@nictiz.nl>
2020-07-05 13:26:52 +0200

    Prepare for Nictiz run

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# sample-ig
-A sample, template-driven implementation guide that provides a starting environment to use a base for defining new IGs
+# Nictiz-FHIR-IG
+Experimental repository to test conformance resources/examples with
+
+## Purpose
+Quality assurance testing of conformance resources in a branch of repository [Nictiz-STU3-Zib2017](https://github.com/Nictiz/Nictiz-STU3-Zib2017)
+
+## How to run
+Clone this repo next to wherever you have cloned Nictiz-STU3-Zib2017. Run _genonce.sh if you are on Linux/Unix/macOS. If you are on Windows you could run _genonce.bat but at time of writing this script is not as complete as _genonce.sh.
+
+The _genonce.sh script will get examples and conformance resources from whatever branch you have in Nictiz-STU3-Zib2017 and run the IG Publisher. The _genonce.bat script will only run the IG Publisher with what is already in there.
+
+## Background
+This repository is based on the project [HL7 FHIR sample-ig](https://github.com/FHIR/sample-ig). FHIR is © and ® HL7. See the HL7 FHIR [license](http://hl7.org/fhir/license.html) for more details.
--- _genonce.sh
+++ _genonce.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 publisher_jar=publisher.jar
 input_cache_path=./input-cache/
+
+#### 2020-01-01 Nictiz additions ####
+echo Deleting .DS_Store files
+find . -name ".DS_Store" -exec rm {} \;
+
+nictiz_input_source=../Nictiz-STU3-Zib2017
+if [ -e $nictiz_input_source ]; then
+    echo Refreshing source from:
+    git -C $nictiz_input_source status
+    echo ""
+    
+    echo Refresh examples from checked out Git branch
+    rm -rf input/examples/*
+    find $nictiz_input_source/Examples -name "*.xml" -exec cp {} input/examples/ \;
+    
+    echo Refresh conformance resources from checked out Git branch
+    rm -rf input/resources/*
+    find $nictiz_input_source/Profiles\ -\ ZIB\ 2017 -name "*.xml" -exec cp {} input/resources/ \;
+    
+    echo Creating IG from input
+    if [ -e input/myig.xml ]; then 
+        mv input/myig.xml input/myig-old.xml
+    fi
+    
+    java -cp $input_cache_path/$publisher_jar net.sf.saxon.Transform -xsl:build-myig.xsl -s:build-myig.xsl -o:input/myig.xml
+else
+    echo Cannot refresh conformance resources. Nictiz IG Publisher is not next to $nictiz_input_source
+fi
+
+#### 2020-01-01 Nictiz additions ####
+
 echo Checking internet connection...
 curl -sSf tx.fhir.org > /dev/null
 
@@ -16,12 +47,12 @@ echo "$txoption"
 
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
-	java -jar $publisher -ig ig.ini $txoption $*
+	java -Xmx10G -jar $publisher -ig ig.ini $txoption $*
 
 else
 	publisher=../$publisher_jar
 	if test -f "$publisher"; then
-		java -jar $publisher -ig ig.ini $txoption $*
+		java -Xmx10G -jar $publisher -ig ig.ini $txoption $*
 	else
 		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
 	fi
